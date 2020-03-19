d590a842f763ebd992b7a11b03711cd38d39a3ec
Charles Finocchiaro <56731501+null267@users.noreply.github.com>
2020-03-19 18:24:54 -0400

    Update expenseModel.js

--- server/expenseModel.js
+++ server/expenseModel.js
@@ -1,5 +1,5 @@
 const { Pool } = require('pg');
-const myURI = 'postgres://eivedciv:NvP9zaGxjmWQCv5_JCwuJ-uy1fnb4hAX@drona.db.elephantsql.com:5432/eivedciv';
+const myURI = '';
 const pool = new Pool({ connectionString: myURI });
 
 // const queryStr = 'CREATE TABLE IF NOT EXISTS accounts (id SERIAL PRIMARY KEY, userName VARCHAR NOT NULL, hashpass VARCHAR);'
